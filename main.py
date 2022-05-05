2b604f3d1612962722b4a3075427e1351b216c2b
Kaio Antônio <kaioaar@gmail.com>
2022-05-04 23:16:34 -0300

    adds: allow methods

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user_router.router)
+origins = ["*"]
+
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=['*']
-    )
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def Home():
