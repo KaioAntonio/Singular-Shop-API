ea0e86511c80f2128837888aeef1ca71d3b9aabd
Kaio Antonio <kaioaar@gmail.com>
2022-05-25 18:26:14 -0300

    refactor: error mensages and status

--- routers/user_router.py
+++ routers/user_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,  HTTPException
+from fastapi import APIRouter,  HTTPException, Body
 from models.user import *
 from db.config import *
 from fastapi_login import LoginManager
@@ -6,58 +6,99 @@ from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 SECRET = 'G@LATIKA!MAT' 
-
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+responses_custom = {
+        422: {
+            "content": {
+                "application/json": {
+                    "example": {"status": 422, "message": "validation error"}
+                }
+            },
+        },
+        200: {
+            "content": {
+                "application/json": {
+                    "example": {"status": 200, "message": "update sucess"}
+                }
+            },
+        },
+    }
 
 
-@router.post("/v1/user", tags=["User"], description="Creates new user")
+@router.post("/v1/user", tags=["User"], description="Creates new user", responses= responses_custom)
 def create_user(new_user: User):
     user_exist = load_user(new_user.email)
     if len(new_user.password) < 8:
-        return {'error': 'password must be longer than 8 characters'}
+        return {'error': 'Senha não pode ter menos que 8 caracteres'}
 
     if new_user.password.islower():
-        return {'error': 'password must have at least one capital letter'}
+        return {'error': 'Senha precisa ter letra maiúscula'}
 
     if new_user.password.isalpha():
-        return {'error': 'password need a number'}
+        return {'error': 'Senha precisa de um número'}
     
     if new_user.password.isalnum():
-        return {'error': 'password need a special character'}
+        return {'error': 'Senha precisa de um caracter especial'}
 
     if user_exist:
-        return {'error': 'email is already token'}
+        return {'status': 422, 'error': 'Este e-mail já está sendo utilizado'}
 
     elif new_user.email == "":
-        return {'error': 'email can not be null'}
+        return {'status': 422, 'error': 'E-mail inválido'}
         
     else:
         new_user.insert_user(new_user.username,new_user.password,new_user.email,new_user.admin,new_user.avatar)
         return new_user
 
-@router.get("/v1/user", tags=["User"], description= "Reads all users")
+@router.get("/v1/user", tags=["User"], description= "Reads all users",
+     responses={
+        200: {
+            "description": "User requested",
+            "content": {
+                "application/json": {
+                    "example": { "user_id": "42e8db92-e792-46fd-8344-b3cc4e49a49d",
+                                "username": "example",
+                                "password": "$2b$12$.32oFz6fGEYNHauOFnrrWuSInKwAoDksKoDWkVQNeDjuiozI8i8pO",
+                                "email": "example@example.com",
+                                "admin": "true",
+                                "avatar": "https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2016/07/fa.png"
+                                }
+                }
+            },
+        },
+    },)
 def get_all_users():
     user = User()
     return user.read_user()
 
-
-@router.put("/v1/user", tags=["User"], description= "Update user")
+@router.put("/v1/user", tags=["User"], description= "Update user by email", responses=responses_custom)
 def put_user(new_user: User):
-    new_user.put_user(new_user.username, new_user.password, new_user.admin, new_user.avatar, new_user.email)
-    return new_user
+    user_exist = load_user(new_user.email)
+    if user_exist:
+        new_user.put_user(new_user.username, new_user.password, new_user.admin, new_user.avatar, new_user.email)
+        return {"status": 200, "message": "update sucess"}
+    else:
+        return {"status": 422, "message": "validation error"}
 
-@router.delete("/v1/user/{email}", tags=["User"], description= "Delete user")
+@router.delete("/v1/user/{email}", tags=["User"], description= "Delete user by email", responses= responses_custom)
 def delete_user(email: str):
-    user = User()
-    user.delete_user(email)
-    print(user)
-    return {"message": "user delete with sucess"}
+    user_exist = load_user(email)
+    if user_exist:
+        user = User()
+        user.delete_user(email)
+        return {"status": 200, "message": "user delete with sucess"}
+    else:
+        return {"status": 422, "message": "validation error"}
 
-@router.get("/v1/user/{email}", tags=["User"], description="Reads a user")
+@router.get("/v1/user/{email}", tags=["User"], description="Reads a user", responses= responses_custom)
 def get_user_by_email(email: str):
-    user = User()
-    return user.find_by_id_user(email)[0]
+    user_exist = load_user(email)
+    if user_exist:
+        user = User()
+        return user.find_by_id_user(email)[0]
+    else:
+        return {"status": 422, "message": "validation error"}
 
 manager = LoginManager(SECRET, token_url='/auth/token')
 
@@ -65,9 +106,13 @@ def load_user(email: str):
     user = User()
     return user.find_by_id_user(email)
 
-@router.post('/v1/login/', tags=["Login"], description = "Autenticated Token")
-def login(user: User):
-
+@router.post('/v1/login/', tags=["Login"], description = "Autenticated Token", responses=responses_custom)
+def login(user: User = Body(
+        default={
+            "email": "test@test.com",
+            "password": "@test1234"
+        }
+    )):
     email = user.email
     password = user.password
     set_user = User.find_password(email)
@@ -75,10 +120,10 @@ def login(user: User):
     user = load_user(email)
 
     if not user:
-        raise HTTPException(status_code=404, detail= 'User or Password invalid')
+        raise HTTPException(status_code=404, detail= 'Usuário ou senha inválido')
 
     elif Hasher.verify_password(password,set_user[0][0]) == False:
-        raise HTTPException(status_code=404, detail= 'User or Password invalid')
+        raise HTTPException(status_code=404, detail= 'Usuário ou senha inválido')
 
     access_token = manager.create_access_token(
         data=dict(sub=email)
@@ -86,7 +131,10 @@ def login(user: User):
 
     return {'access_token': access_token, 'token_type': 'bearer', 'admin': admin}
 
-@router.patch('/v1/avatar/', tags=["User"], description="Patch a new avatar")
+@router.patch('/v1/avatar/', tags=["User"], description="Patch a new avatar", responses=responses_custom)
 def patch_avatar(user: User):
-    new_avatar = user.patch_new_avatar(user.email,user.avatar)
-    return {"message": "Update avatar with sucess!"}
+    if user_exist:
+        new_avatar = user.patch_new_avatar(user.email,user.avatar)
+        return {"status": 200, "message": "Update avatar with sucess!"}
+    else:
+        return {"status": 422, "message": "validation error"}
